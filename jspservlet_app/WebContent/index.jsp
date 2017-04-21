c13e42c7cea4388b1633117cc836aa88350c0ed9
anand <anand8412@gmail.com>
2017-04-21 09:36:55 +0530

    added login functionality

--- jspservlet_app/WebContent/index.jsp
+++ jspservlet_app/WebContent/index.jsp
@@ -1,3 +1,19 @@
 <html>
-Welcome to JSP-SERVLET Project
+Welcome to JSP-SERVLET Project bjkhkjhkjh
+
+
+<form action="LoginServlet">
+  user name:<br>
+  <input type="text" name="xyz" >
+  <br>
+  Password:<br>
+  <input type="password" name="password">
+  <br><br>
+  <input type="submit" value="Login">
+</form> 
+
+
+
+
+
 </html>
--- jspservlet_app/src/com/jspserv/servlets/LoginServlet.java
+++ jspservlet_app/src/com/jspserv/servlets/LoginServlet.java
@@ -7,19 +7,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/WelcomeServlet")
-public class WelcomeServlet extends HttpServlet {
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+
+	public LoginServlet() {
+		super();
+
+	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("welcome tservlet");
-  request.getRequestDispatcher("index.jsp").include(request, response);
+
+		String name = request.getParameter("xyz");
+		String password = request.getParameter("password");
+
+		if (name.equals("anand") && password.equals("anand")) {
+			request.getRequestDispatcher("success.jsp").include(request, response);
+		} else {
+			request.getRequestDispatcher("failure.jsp").include(request, response);
+		}
 
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
 		doGet(request, response);
 	}
 
